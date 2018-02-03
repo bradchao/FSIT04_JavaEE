0e6d00eaf3257a3fd05a6f9275f22002b6512c03
user <user@DESKTOP-C3T5D5F>
2018-02-03 12:09:36 +0800

    201802031209

--- j2ee/src/tw/brad/j2ee/Brad05.java
+++ j2ee/src/tw/brad/j2ee/Brad05.java
@@ -2,6 +2,7 @@ package tw.brad.j2ee;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Enumeration;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -26,7 +27,20 @@ public class Brad05 extends HttpServlet {
 		
 		response.setContentType("text/html; charset=UTF-8");
 		PrintWriter pw = response.getWriter();
-		pw.write("Hello, 您好:" + what);
+		pw.write("Hello, 您好:" + what + "<br />");
+		
+		Enumeration<String> params = request.getParameterNames();
+		while (params.hasMoreElements()) {
+			String key = params.nextElement();
+			String value = request.getParameter(key);
+			pw.write(key + ":" + value  + "<br />");
+			System.out.println(value);
+		}
+		
+		
+		
+		
+		
 		pw.flush();
 		pw.close();
 		
