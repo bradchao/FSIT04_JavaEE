aaed576bf0747cefb90656d664373f13f5172540
user <user@DESKTOP-C3T5D5F>
2018-02-03 14:10:28 +0800

    201802031410

--- j2ee/src/tw/brad/j2ee/Brad07.java
+++ j2ee/src/tw/brad/j2ee/Brad07.java
@@ -15,14 +15,29 @@ public class Brad07 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		int x = Integer.parseInt(request.getParameter("a"));
-		int y = Integer.parseInt(request.getParameter("y"));
+		int x, y, op, result;
+		x = y = op = result = 0;
+		String opString = "?";
 		
-		int result = x + y;
+		try {
+			x = Integer.parseInt(request.getParameter("x"));
+			y = Integer.parseInt(request.getParameter("y"));
+			op = Integer.parseInt(request.getParameter("op"));
+			
+			switch (op) {
+				case 1: result = x + y; opString="+"; break;
+				case 2: result = x - y; opString="-"; break;
+				case 3: result = x * y; opString="x"; break;
+				case 4: result = x / y; opString="/"; break;
+			}
+			
+		}catch (Exception e) {
+			
+		}
 		
 		response.setContentType("text/html; charset=UTF-8");
 		PrintWriter pw = response.getWriter();
-		pw.write(x + " + " + y + " = " + result);
+		pw.write(x + opString + y + " = " + result);
 		pw.flush();
 		pw.close();
 		
