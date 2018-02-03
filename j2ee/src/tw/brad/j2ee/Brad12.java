c1cc891fd7482db9281a39c9db5393e60a9c3e30
user <user@DESKTOP-C3T5D5F>
2018-02-03 16:07:37 +0800

    201802031607

--- j2ee/src/tw/brad/j2ee/Brad12.java
+++ j2ee/src/tw/brad/j2ee/Brad12.java
@@ -1,10 +1,13 @@
 package tw.brad.j2ee;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Collection;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,18 +15,36 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 
 @WebServlet("/Brad12")
+@MultipartConfig
 public class Brad12 extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html; charset=UTF-8");
 		PrintWriter pw = response.getWriter();
 		
-		Collection<Part> parts = request.getParts();
-		for (Part part : parts) {
-			String filename = part.getSubmittedFileName();
-			pw.write(filename);
-		}
+		String uploadPath = getServletContext().getInitParameter("file-upload");
+		System.out.println(uploadPath);
+		
+		Part upload = request.getPart("upload");
+		String filename = upload.getSubmittedFileName();
+		long size = upload.getSize();
+		pw.write(filename + ":" + size);
+		
+		//byte[] buf = new byte[(int)size];
+		BufferedInputStream bin = 
+				new BufferedInputStream(upload.getInputStream()); 
+		byte[] buf = bin.readAllBytes();
+		bin.close();
+		
+		FileOutputStream fout = 
+			new FileOutputStream(new File(uploadPath, filename));
+		fout.write(buf);
+		fout.flush();
+		fout.close();
 		
 		
-	
+		
+		
+		
+		
 	}
 }
