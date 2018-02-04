76002fb874477970f705d3e821854d3a3177d754
user <user@DESKTOP-C3T5D5F>
2018-02-04 14:50:18 +0800

    201802041450

--- j2ee/src/tw/brad/j2ee/Brad23.java
+++ j2ee/src/tw/brad/j2ee/Brad23.java
@@ -2,6 +2,9 @@ package tw.brad.j2ee;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -16,13 +19,29 @@ public class Brad23 extends HttpServlet {
 		response.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = response.getWriter();
 		
-		testMySQL();
+		//request.setCharacterEncoding("UTF-8");
+		
+		String account = request.getParameter("account");
+		String passwd = request.getParameter("passwd");
+		String realname = request.getParameter("realname");
+		
+		if (account != null) {
+			testMySQL(account, passwd, realname);
+		}
 	}
 
-	private void testMySQL() {
+	private void testMySQL(String account, String passwd, String realname) {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			System.out.println("OK");
+			Connection conn = 
+				DriverManager.getConnection(
+					"jdbc:mysql://localhost/brad", "root", "root");
+			Statement stmt = conn.createStatement();
+			String sql = "INSERT INTO member (account,passwd,realname)" +
+					" VALUES ('" + account + "','" + passwd + "','" + realname +"')";
+			stmt.executeUpdate(sql);
+			stmt.close();
+			
 		}catch(Exception e) {
 			System.out.println(e.toString());
 		}
